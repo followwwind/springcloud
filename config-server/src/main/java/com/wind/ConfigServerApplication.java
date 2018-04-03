c035f0c555a48cd8537912ab00487cf30343a4ba
followwwind <18771933975@163.com>
2018-04-03 18:19:29 +0800

    添加服务链路追踪(Spring Cloud Sleuth)

---
+++ config-server/src/main/java/com/wind/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.wind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wind
+ * http请求地址和资源文件映射如下:
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
